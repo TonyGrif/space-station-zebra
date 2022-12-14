2ab2d7a29bdf45dad749f80d8a23ece71e38f878
TonyGrif <TonyGriffin2000@gmail.com>
2022-12-13 20:45:57 -0500

    part files

---
+++ Station/main/cpp/part.cpp
@@ -0,0 +1,41 @@
+#include "../headers/part.h"
+
+Part::Part()
+{
+    this->PartId(1);
+    this->IsBroken(false);
+}
+
+Part::Part(int id, bool b)
+{
+    this->PartId(id);
+    this->IsBroken(b);
+}
+
+bool Part::operator==(const Part& rhs) const
+{
+    if(this->PartId() == rhs.PartId() && this->IsBroken() == rhs.IsBroken()) 
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Part::operator<(const Part& rhs) const
+{
+    if(this->PartId() != rhs.PartId()) {
+        return (this->PartId() < rhs.PartId());
+    }
+}
+
+std::string Part::toString() const
+{
+    std::string temp;
+
+    temp.append("Part #");
+    temp += std::to_string(this->PartId());
+    temp.append(" - ");
+    temp += std::to_string(this->IsBroken());
+
+    return temp;
+}
