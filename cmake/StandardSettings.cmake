2f30ec141c01b68b8b96b1b5684c577f457267d9
TonyGrif <TonyGriffin2000@gmail.com>
2023-01-05 21:02:08 -0500

    multiple build targets based on needs

---
+++ cmake/StandardSettings.cmake
@@ -0,0 +1,4 @@
+option(BUILD_EXECUTABLE "Build the project as an executable." ON)
+option(BUILD_HEADERS_ONLY "Build the project as a header-only library." OFF)
+
+option(ENABLE_UNIT_TESTING "Enable unit tests for this project." ON)
